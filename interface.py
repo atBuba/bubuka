f32ee8f511112541c352f310ba5abae8a7d70828
atBuba <123889924+atBuba@users.noreply.github.com>
2024-09-16 07:17:15 +0300

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template
+from transformers import BertTokenizer, BertForSequenceClassification
+import torch
+
+app = Flask(__name__)
+
+# Загрузка модели и токенизатора
+model = BertForSequenceClassification.from_pretrained('/Users/nikitaklenskij/Documents/programs/bubuka/model')
+tokenizer = BertTokenizer.from_pretrained('/Users/nikitaklenskij/Documents/programs/bubuka/model')
+
+
+# Категории
+categories = ['Грусть/Печаль/Страдания', 'Оптимизм/Веселье/Счастье', 'Новый год/Рождество', 
+        'Любовь/Романтика', 'Патриотическая песня', 'Лето', 'Осень'
+]
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        text = request.form['text']
+        inputs = tokenizer(text, return_tensors='pt', truncation=True, padding=True, max_length=512)
+        with torch.no_grad():
+            outputs = model(**inputs)
+            logits = outputs.logits
+            probabilities = torch.sigmoid(logits).cpu().numpy()[0]
+        results = {cat: prob for cat, prob in zip(categories, probabilities)}
+        sorted_results = dict(sorted(results.items(), key=lambda item: item[1], reverse=True))
+        return render_template('index.html', text=text, results=sorted_results)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
